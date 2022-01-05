3c75ab511009c467a5ae4b8be8663c515f70fe5a
Wesley Feitosa <jwesleydasilva@gmail.com>
2022-01-04 22:06:55 -0300

    feat: criando header e pagina de carros

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
 
-import { Home } from "./src/screens/Home";
+import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
--- rentx/src/screens/Home/index.tsx
+++ rentx/src/screens/Home/index.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { ReactElement } from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from "./styles";
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
-export function Home() {
+import { Container, Header, HeaderContent, TotalCars } from './styles';
+
+export function Home(): ReactElement {
+  const carData = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail:
+      'https://mediaservice.audi.com/media/live/50900/fly1400x601n1/f5f/2021.png?wid=850',
+  };
+
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carData} />
     </Container>
   );
 }
